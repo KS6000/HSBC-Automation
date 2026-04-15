4d21703eb0f22c8849c084d3f2094f66c6a2875a
KS6000 <kshura600@gmail.com>
2026-04-15 09:56:35 +0100

    fix: updated CreditCardPage selectors to match live HSBC site

--- cypress/pages/CreditCardPage.js
+++ cypress/pages/CreditCardPage.js
@@ -3,40 +3,28 @@ class CreditCardPage {
     cy.visit('https://www.hsbc.co.in/')
   }
 
-  bankingmenu() {
-    return cy.contains('banking').first()
-  }
-
-  creditCardsLink() {
-    return cy.contains('Credit Cards').first()
-  }
-
   creditCardsHeader() {
-    return cy.contains('Credit Cards').first()
+  return cy.contains('h1, h2, div', 'Credit cards')
   }
 
   applyNowButton() {
-    return cy.contains('Apply now').first()
+    return cy.get('body').contains('Apply', {matchcase: false })
   }
 
-  cardsSection() {
-    return cy.get('body')
+  compareCardsButton() {
+    return cy.contains('Compare cards').first()
   }
 
   comparePopupHeader() {
     return cy.contains('Select cards to compare').first()
   }
 
-  closeButton() {
-    return cy.contains('Close').first()
-  }
-
   compareButton() {
     return cy.contains('Compare').first()
   }
 
-  addCardsLink() {
-    return cy.contains('Add cards').first()
+  closeButton() {
+    return cy.contains('Close').first()
   }
 }
 
